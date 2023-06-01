4bbe3291041328f8d1ab1841fb4f7a00297bed9a
knowave <kwaktaemin98@gmail.com>
2023-06-01 16:10:49 +0900

    Number Data Type

---
+++ src/main/java/DataType.java
@@ -0,0 +1,7 @@
+public class DataTypeStr {
+    public static void main(String[] args) {
+        // write your code here
+        char c = 'A';
+        System.out.println(c);
+    }
+}
---
+++ src/main/java/DataTypeNum.java
@@ -0,0 +1,19 @@
+public class DataTypeNum {
+    public static void main(String[] args) {
+        // write your code here
+        short s = 1;
+        System.out.println(s);
+
+        int i = 3;
+        System.out.println(i);
+
+        long l = 4;
+        System.out.println(l);
+
+        float f = 5.5F;
+        System.out.println(f);
+
+        double d = 6.6;
+        System.out.println(d);
+    }
+}
