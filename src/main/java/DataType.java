19b3f322bb00526b35405c77783d243b9530d2f2
knowave <kwaktaemin98@gmail.com>
2023-06-01 16:11:08 +0900

    문자, 논리, 바이트

--- src/main/java/DataType.java
+++ src/main/java/DataType.java
@@ -1,7 +1,14 @@
-public class DataTypeStr {
+public class DataType {
     public static void main(String[] args) {
         // write your code here
         char c = 'A';
         System.out.println(c);
+
+        boolean fact = true;
+        fact = false;
+        System.out.println(fact);
+
+        byte data = 'd';
+        System.out.println(data);
     }
 }
