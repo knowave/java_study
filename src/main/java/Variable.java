94f9922eeef306d2b1aaf1112f9dfd7b0b5d9b4f
knowave <kwaktaemin98@gmail.com>
2023-06-01 15:40:38 +0900

    변수와 상수

---
+++ src/main/java/Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+    public static void main(String[] args) {
+        // write your code here
+        int number = 5;
+        System.out.println(number);
+
+        String sparta = "Hello Sparta!";
+        System.out.println(sparta);
+
+        String str = "Test";
+        System.out.println(str);
+
+        final int finalNumber = 10;
+        System.out.println(finalNumber);
+
+        sparta = "Good Bye Sparta";
+        System.out.println(sparta);
+    }
+}
