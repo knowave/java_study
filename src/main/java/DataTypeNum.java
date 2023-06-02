e63f807acf48cbe4fe934b794766bbd5507519d6
knowave <kwaktaemin98@gmail.com>
2023-06-02 13:43:55 +0900

    숫자 자료형 MAX, MIN 값 Print

--- src/main/java/DataTypeNum.java
+++ src/main/java/DataTypeNum.java
@@ -15,5 +15,17 @@ public class DataTypeNum {
 
         double d = 6.6;
         System.out.println(d);
+
+        // 아래와 같이 각 자료형의 MAX, MIN 값을 가져올 수 있다.
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
     }
 }
