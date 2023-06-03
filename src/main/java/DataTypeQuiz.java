4505f76f04a9c75e6a1405ea67be090448644152
knowave <kwaktaemin98@gmail.com>
2023-06-03 10:51:29 +0900

    Data Type Quiz

---
+++ src/main/java/DataTypeQuiz.java
@@ -0,0 +1,13 @@
+public class DataTypeQuiz {
+    public static void main(String[] args) {
+        // write your code here
+        char s = 's';
+        System.out.println(s);
+
+        int ten = 10;
+        System.out.println(ten);
+
+        int[] intArray = new int[] {10, 20, 30};
+        System.out.println(intArray[intArray.length -1]);
+    }
+}
