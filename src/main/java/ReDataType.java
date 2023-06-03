7f587a2ef4090dd8095bf022f80feb44eee16e4e
knowave <kwaktaemin98@gmail.com>
2023-06-03 10:32:08 +0900

    Reference Type

--- src/main/java/ReDataType.java
+++ src/main/java/ReDataType.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ReDataType {
+import java.util.Arrays;
+
+public class ReDataType {
+    public static void main(String[] args) {
+        // write your code here
+        String sparta = "Hello Sparta";
+        System.out.println(sparta);
+
+        int[] intEmptyArray = new int[5];
+        System.out.println(Arrays.toString(intEmptyArray));
+
+        int[] intArray = new int[] {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(intArray));  // [0, 0, 0, 0, 0]
+
+        String[] stringEmptyArrays = new String[5];
+        System.out.println(Arrays.toString(stringEmptyArrays)); // [null, null, null, null, null]
+
+        String[] season = {"봄", "여름", "가을", "겨울"};
+        System.out.println(Arrays.toString(season));
+    }
 }
