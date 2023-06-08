bd6648b2e0a1e95e4534ca69dcbbe566adab4e5a
knowave <kwaktaemin98@gmail.com>
2023-06-08 15:35:38 +0900

    연산자 퀴즈

---
+++ src/main/java/OperatorQuiz.java
@@ -0,0 +1,17 @@
+public class OperatorQuiz {
+    public static void main(String[] args) {
+        // write your code here
+        int num1 = 10;
+        int num2 = 3;
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        num1 += num1;
+        num2 *= num1;
+
+        System.out.println(num1);   // 20
+        System.out.println(num2);   // 60
+        System.out.println(bool1 && bool2);  // false
+    }
+}
