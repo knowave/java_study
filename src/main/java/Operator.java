5d5efa12aa34dd4b6604ddb2603022dbb3b88218
knowave <kwaktaemin98@gmail.com>
2023-06-08 14:37:00 +0900

    int Operator

---
+++ src/main/java/Operator.java
@@ -0,0 +1,13 @@
+public class Operator {
+    public static void main(String[] args) {
+        // write your code here
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
