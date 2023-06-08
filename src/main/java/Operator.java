d61373aa905e02b599353c848e1b808a550476d0
knowave <kwaktaemin98@gmail.com>
2023-06-08 14:48:59 +0900

    대입 연산자 추가

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -9,5 +9,21 @@ public class Operator {
         System.out.println(num1 * num2);
         System.out.println(num1 / num2);
         System.out.println(num1 % num2);
+        System.out.println(num1);
+
+        num1 += num2;
+        System.out.println(num1);   // 15
+
+        num1 -= num2;
+        System.out.println(num1);   // 10 (num1은 윗 줄에서 이미 num2랑 더한 값의 결과를 num1에 값을 할당해줬기 때문에 num1은 15였으니까 결과는 10.)
+
+        num1 *= num2;
+        System.out.println(num1);   // 50
+
+        num1 /= num2;
+        System.out.println(num1);   // 10
+
+        num1 %= num2;
+        System.out.println(num1);   // 0
     }
 }
