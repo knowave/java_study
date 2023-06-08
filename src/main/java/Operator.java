493cb8ff1f4adb5e3921883cb03a0c079de7ee18
knowave <kwaktaemin98@gmail.com>
2023-06-08 15:30:07 +0900

    연산자 주석 제거

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -1,9 +1,11 @@
 public class Operator {
     public static void main(String[] args) {
         // write your code here
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = 10;
         boolean a = true;
         boolean b = false;
-
 
 
         /* 논리 연산자 */
@@ -11,38 +13,38 @@ public class Operator {
         System.out.println(a && true);  // true
         System.out.println(a || b); // true
         System.out.println(false || b); // false
-        System.out.println(!b); // true
         System.out.println(!a); // false
+        System.out.println(!b); // true
 
         /* 관계 연산자 */
-//        System.out.println(num1 > num2);    // true
-//        System.out.println(num1 >= num3);   // true
-//        System.out.println(num1 < num2);    // false
-//        System.out.println(num1 <= num3);   // true
-//        System.out.println(num1 == num3);   // true
+        System.out.println(num1 > num2);    // true
+        System.out.println(num1 >= num3);   // true
+        System.out.println(num1 < num2);    // false
+        System.out.println(num1 <= num3);   // true
+        System.out.println(num1 == num3);   // true
 
         /* 산술 연산자 */
-//        System.out.println(num1 + num2);
-//        System.out.println(num1 - num2);
-//        System.out.println(num1 * num2);
-//        System.out.println(num1 / num2);
-//        System.out.println(num1 % num2);
-//        System.out.println(num1);
-//
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+        System.out.println(num1);
+
         /* 대입 연산자 */
-//        num1 += num2;
-//        System.out.println(num1);   // 15
-//
-//        num1 -= num2;
-//        System.out.println(num1);   // 10 (num1은 윗 줄에서 이미 num2랑 더한 값의 결과를 num1에 값을 할당해줬기 때문에 num1은 15였으니까 결과는 10.)
-//
-//        num1 *= num2;
-//        System.out.println(num1);   // 50
-//
-//        num1 /= num2;
-//        System.out.println(num1);   // 10
-//
-//        num1 %= num2;
-//        System.out.println(num1);   // 0
+        num1 += num2;
+        System.out.println(num1);   // 15
+
+        num1 -= num2;
+        System.out.println(num1);   // 10 (num1은 윗 줄에서 이미 num2랑 더한 값의 결과를 num1에 값을 할당해줬기 때문에 num1은 15였으니까 결과는 10.)
+
+        num1 *= num2;
+        System.out.println(num1);   // 50
+
+        num1 /= num2;
+        System.out.println(num1);   // 10
+
+        num1 %= num2;
+        System.out.println(num1);   // 0
     }
 }
