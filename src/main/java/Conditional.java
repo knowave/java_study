1cfb139fd0990cd783892465015e25ec7bdb24dc
knowave <kwaktaemin98@gmail.com>
2023-06-09 14:16:08 +0900

    if문과 else if문

---
+++ src/main/java/Conditional.java
@@ -0,0 +1,13 @@
+public class Conditional {
+    public static void main(String[] args) {
+        // write your code here
+        int check = 100;
+        int num1 = 51;
+
+        if (num1> check) {
+            System.out.println("100보다 큰 수 입니다.");
+        } else if (num1 > 50) {
+            System.out.println("50보다 큰 수 입니다. 또는 100보다 작거나 같습니다.");
+        }
+    }
+}
