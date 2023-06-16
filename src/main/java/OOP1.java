0eea46a8689c3cbbb7fe91b615a7e5a7caf33f83
knowave <kwaktaemin98@gmail.com>
2023-06-16 15:49:33 +0900

    class에서 method를 사용하기

--- src/main/java/OOP1.java
+++ src/main/java/OOP1.java
@@ -1,23 +1,21 @@
-class Phone {
-    String model;
-    String color;
-    int price;
+class Calculation {
+    int add(int x, int y) {
+        return x + y;
+    }
+
+    int subtract(int x, int y) {
+        return x - y;
+    }
 }
 
 public class OOP1 {
     public static void main(String[] args) {
-        Phone galaxy = new Phone();
-        galaxy.model = "Galaxy10";
-        galaxy.color = "Black";
-        galaxy.price = 100;
+        // write your code here
+        Calculation calculation = new Calculation();
+        int addResult = calculation.add(1, 2);
+        int subtractResult = calculation.subtract(2, 1);
 
-        Phone iphone =new Phone();
-        iphone.model = "iPhoneX";
-        iphone.color = "Black";
-        iphone.price = 200;
-
-
-        System.out.println("철수는 이번에 " + galaxy.model + galaxy.color + " + 색상을 " + galaxy.price + "만원에 샀다.");
-        System.out.println("영희는 이번에 " + iphone.model + iphone.color + " + 색상을 " + iphone.price + "만원에 샀다.");
+        System.out.println(addResult);
+        System.out.println(subtractResult);
     }
 }
