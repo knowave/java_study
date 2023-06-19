c31b239cfc5fc4fc369f915498e932ffb068e9a2
knowave <kwaktaemin98@gmail.com>
2023-06-19 18:30:45 +0900

    퀴즈 구현할 class 생성

---
+++ src/main/java/GrandParent.java
@@ -0,0 +1,14 @@
+public class GrandParent extends Person implements Walkable{
+    public GrandParent(String name, int age) {
+        super(name, age, 1);
+    }
+
+    @Override
+    public void walk(int x, int y) {
+        printWhoAmI();
+        System.out.println("walk speed: " + speed);
+        this.x = x;
+        this.y = y;
+        System.out.println("Walk to " + getLocation());
+    }
+}
