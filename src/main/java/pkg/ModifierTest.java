831526fd6aa9f95e7b083ec6b38ea42661d7adff
knowave <kwaktaemin98@gmail.com>
2023-06-19 17:04:08 +0900

    create package

--- src/main/java/pkg/ModifierTest.java
+++ src/main/java/pkg/ModifierTest.java
@@ -1,2 +1,16 @@
-package pkg;public class ModifierTest {
+package pkg;
+
+public class ModifierTest {
+    private void messageInside() {
+        System.out.println("This is private Modifier");
+    }
+
+    public void messageOutside() {
+        System.out.println("This is public Modifier");
+        messageInside();
+    }
+
+    protected void messageProtected() {
+        System.out.println("This is protected Modifier");
+    }
 }
